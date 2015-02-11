c3a2eaf47ca50bbf6d5da81ab1fc1860bf2fb25d
Brian Zerangue <brian.zerangue@gmail.com>
2015-02-11 17:04:44 -0600

    initial commit

---
+++ v1/js/Welcome.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 11:22:36 Dec 20, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:33:38 Feb 11, 2015.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+// Created by iWeb 3.0.2 local-build-20110209
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00}),stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,265),url:'Welcome_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Welcome_files/stroke_1.png'},{rect:new IWRect(1,-1,628,2),url:'Welcome_files/stroke_2.png'},{rect:new IWRect(629,-1,2,2),url:'Welcome_files/stroke_3.png'},{rect:new IWRect(629,1,2,265),url:'Welcome_files/stroke_4.png'},{rect:new IWRect(629,266,2,2),url:'Welcome_files/stroke_5.png'},{rect:new IWRect(1,266,628,2),url:'Welcome_files/stroke_6.png'},{rect:new IWRect(-1,266,2,2),url:'Welcome_files/stroke_7.png'}],new IWSize(630,267))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Welcome_files/WelcomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');fixupIECSS3Opacity('id4');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
